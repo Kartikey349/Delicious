a7d2be12dd5c39bd7de0ed0fa4f69285d29f892d
kartikey sharma <kartikey7518@gmail.com>
2025-07-15 12:16:31 +0530

    resolve delivery time error

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -11,18 +11,19 @@ export const RestaurantCard = (props) => {
         name,
         cuisines,
         cloudinaryImageId,
-        deliveryTime,
+        sla,
         costForTwo,
         avgRating
     } = resData.info;
+
     
     return (
         <div className="m-4 p-4 w-[250px] h-[400px] shadow-md rounded-md transform transition duration-300 hover:scale-95">
             <img className="res-logo w-[100%] h-[180px] rounded-md" src={ CDN_URL + cloudinaryImageId} />
 
             <h3 className="font-bold my-2 leading-5">{name}</h3>
-            <h4>{cuisines.join(", ")}</h4>
-            <h4>{deliveryTime} minutes</h4>
+            <h4 className="leading-tight">{cuisines.join(", ")}</h4>
+            <h4>{sla.deliveryTime} minutes</h4>
             <h4>{costForTwo}</h4>
             <button className="bg-green-600 text-white p-1 rounded-sm mt-1 flex items-center gap-1 text-[10px]">
                 <i className="fa-solid fa-star text-white"></i>{avgRating}
