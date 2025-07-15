da341a7007700243dd5321abb0f8ca91b263d6b1
kartikey sharma <kartikey7518@gmail.com>
2025-07-15 13:05:23 +0530

    added front banner with img in restaurant menu

--- src/components/restaurantMenu.js
+++ src/components/restaurantMenu.js
@@ -17,12 +17,15 @@ const RestaurantMenu = () => {
 
     const {name,
         cuisines,
-        costForTwo
+        costForTwo,
+        cloudinaryImageId,
+        avgRating
     } = resInfo?.cards[2]?.card?.card?.info;
+
 
     // const {itemCards} = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[4]?.card?.card;
 
-    // console.log(resInfo)
+    //console.log(resInfo)
 
    const categories = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter((c) => {
         return c.card.card["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
@@ -31,9 +34,21 @@ const RestaurantMenu = () => {
     //console.log(categories)
 
     return(
-        <div className="text-center">
-            <h1 className="text-3xl font-bold mt-10">{name}</h1>
-            <h2 className="my-6 font-semibold">{cuisines.join(", ")}: Rs {costForTwo / 100}</h2>
+        <div className="">
+            <div className="bg-gray-900 p-4 flex w-8/12 m-auto rounded-md">
+                <img className="w-4/12 rounded-sm" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" + cloudinaryImageId} />
+
+                <div className="text-left m-2">
+                    <h1 className="text-3xl  mt-2 text-white ">{name}</h1>
+
+                    <h2 className="my-2 text-white">{cuisines.join(", ")}: Rs {costForTwo / 100}</h2>
+
+                    <button className="bg-green-600 text-white p-1 rounded-sm mt-1 flex items-center gap-1 text-[10px]">
+                        <i className="fa-solid fa-star text-white"></i>{avgRating}
+                    </button>
+
+                </div>
+            </div>
             
             {categories.map((category, index) => {
                 return <RestaurantCategory key={category.card.card.categoryId} data={category.card.card} 
